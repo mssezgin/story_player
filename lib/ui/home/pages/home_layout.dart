05f38321cf24a589aff893c077a1b9043122a1a9
mssezgin <m.68sezgin@gmail.com>
2023-09-11 14:55:43 +0300

    Update dummy home page and layout.

---
+++ lib/ui/home/pages/home_layout.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:story_player/ui/home/pages/home_page.dart';
+
+class HomeLayout extends StatelessWidget {
+  const HomeLayout({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return HomePage(title: title);
+  }
+}
---
+++ lib/ui/home/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Text('Home Page'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Playing stories'),
+            ),
+          );
+        },
+        child: const Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
