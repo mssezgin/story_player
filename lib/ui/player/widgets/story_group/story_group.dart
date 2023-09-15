c3430cfc7ef06ebad525e40f1e6ac1af8ef7a23b
mssezgin <m.68sezgin@gmail.com>
2023-09-15 04:53:04 +0300

    Create gesture detection.

--- lib/ui/player/widgets/story_group/story_group.dart
+++ lib/ui/player/widgets/story_group/story_group.dart
@@ -28,53 +28,63 @@ class StoryGroup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('${user.firstName} ${user.lastName}'),
-        leading: CloseButton(
-          onPressed: onStop,
+    return GestureDetector(
+      onTapDown: (details) {
+        double screenWidth = MediaQuery.of(context).size.width;
+        if (details.globalPosition.dx < screenWidth / 2) {
+          onPlayPreviousStory();
+        } else {
+          onPlayNextStory();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('${user.firstName} ${user.lastName}'),
+          leading: CloseButton(
+            onPressed: onStop,
+          ),
+          actions: [
+            IconButton(
+              onPressed: onPlayPreviousStory,
+              icon: const Icon(Icons.navigate_before),
+            ),
+            IconButton(
+              onPressed: onPause,
+              icon: const Icon(Icons.pause),
+            ),
+            IconButton(
+              onPressed: onPlayNextStory,
+              icon: const Icon(Icons.navigate_next),
+            ),
+          ],
+          elevation: 0,
+          backgroundColor: Colors.black38,
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(4),
+            child: StoryGroupProgressBars(user: user),
+          ),
         ),
-        actions: [
-          IconButton(
-            onPressed: onPlayPreviousStory,
-            icon: const Icon(Icons.navigate_before),
-          ),
-          IconButton(
-            onPressed: onPause,
-            icon: const Icon(Icons.pause),
-          ),
-          IconButton(
-            onPressed: onPlayNextStory,
-            icon: const Icon(Icons.navigate_next),
-          ),
-        ],
-        elevation: 0,
-        backgroundColor: Colors.black38,
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(4),
-          child: StoryGroupProgressBars(user: user),
+        backgroundColor: Colors.black,
+        extendBodyBehindAppBar: true,
+        body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          physics: const NeverScrollableScrollPhysics(),
+          controller: storyController,
+          itemCount: user.stories.length,
+          itemBuilder: (context, index) {
+            switch (user.stories[index].storyType) {
+              case StoryType.image:
+                return Image(
+                  image: NetworkImage(user.stories[index].fileSource),
+                  fit: BoxFit.cover,
+                );
+              case StoryType.video:
+                return const Text('Video');
+              default:
+                return const Text('Unsupported story type.');
+            }
+          },
         ),
-      ),
-      backgroundColor: Colors.black,
-      extendBodyBehindAppBar: true,
-      body: PageView.builder(
-        scrollDirection: Axis.vertical,
-        physics: const NeverScrollableScrollPhysics(),
-        controller: storyController,
-        itemCount: user.stories.length,
-        itemBuilder: (context, index) {
-          switch (user.stories[index].storyType) {
-            case StoryType.image:
-              return Image(
-                image: NetworkImage(user.stories[index].fileSource),
-                fit: BoxFit.cover,
-              );
-            case StoryType.video:
-              return const Text('Video');
-            default:
-              return const Text('Unsupported story type.');
-          }
-        },
       ),
     );
   }
